b48d287f33dad666ad9b5b7d20074c708f406bbb
Tuan Do Xuan <xuantuanncth1@gmail.com>
2024-11-06 07:10:13 +0700

    Add project files.

---
+++ FingerPrinter/Forms/DashBoard.cs
@@ -0,0 +1,38 @@
+using FingerPrinter.Forms;
+
+namespace FingerPrinter
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            Login login_section = new Login();
+            login_section.Show();
+        }
+    }
+}
