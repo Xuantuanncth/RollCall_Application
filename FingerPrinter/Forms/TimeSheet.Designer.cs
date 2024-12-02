646b8381d160b372c629eb34d1cfc8bccd95b89f
Do Xuan Tuan <xuantuanncth1@gmail.com>
2024-12-02 22:04:21 +0700

    Update search data in timesheet

--- FingerPrinter/Forms/TimeSheet.Designer.cs
+++ FingerPrinter/Forms/TimeSheet.Designer.cs
@@ -30,10 +30,21 @@
         {
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            bt_s_month = new Button();
+            cb_month = new ComboBox();
+            label5 = new Label();
+            label4 = new Label();
+            bt_export = new Button();
+            bt_search = new Button();
+            tb_s_date = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            tb_s_name = new TextBox();
+            bt_s_name = new Button();
             label1 = new Label();
-            button2 = new Button();
-            textBox1 = new TextBox();
-            button1 = new Button();
+            bt_s_id = new Button();
+            tb_s_id = new TextBox();
+            bt_s_date = new Button();
             monthCalendar1 = new MonthCalendar();
             dataGridView1 = new DataGridView();
             tabPage2 = new TabPage();
@@ -55,10 +66,21 @@
             // 
             // tabPage1
             // 
+            tabPage1.Controls.Add(bt_s_month);
+            tabPage1.Controls.Add(cb_month);
+            tabPage1.Controls.Add(label5);
+            tabPage1.Controls.Add(label4);
+            tabPage1.Controls.Add(bt_export);
+            tabPage1.Controls.Add(bt_search);
+            tabPage1.Controls.Add(tb_s_date);
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(tb_s_name);
+            tabPage1.Controls.Add(bt_s_name);
             tabPage1.Controls.Add(label1);
-            tabPage1.Controls.Add(button2);
-            tabPage1.Controls.Add(textBox1);
-            tabPage1.Controls.Add(button1);
+            tabPage1.Controls.Add(bt_s_id);
+            tabPage1.Controls.Add(tb_s_id);
+            tabPage1.Controls.Add(bt_s_date);
             tabPage1.Controls.Add(monthCalendar1);
             tabPage1.Controls.Add(dataGridView1);
             tabPage1.Location = new Point(4, 29);
@@ -69,46 +91,147 @@
             tabPage1.Text = "TimeSheet";
             tabPage1.UseVisualStyleBackColor = true;
             // 
+            // bt_s_month
+            // 
+            bt_s_month.Location = new Point(604, 10);
+            bt_s_month.Name = "bt_s_month";
+            bt_s_month.Size = new Size(100, 30);
+            bt_s_month.TabIndex = 16;
+            bt_s_month.Text = "Month";
+            bt_s_month.UseVisualStyleBackColor = true;
+            bt_s_month.Click += bt_s_month_Click;
+            // 
+            // cb_month
+            // 
+            cb_month.FormattingEnabled = true;
+            cb_month.Items.AddRange(new object[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" });
+            cb_month.Location = new Point(426, 12);
+            cb_month.Name = "cb_month";
+            cb_month.Size = new Size(147, 28);
+            cb_month.TabIndex = 15;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(265, 20);
+            label5.Name = "label5";
+            label5.Size = new Size(141, 20);
+            label5.TabIndex = 14;
+            label5.Text = "Search with month";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(736, 80);
+            label4.Name = "label4";
+            label4.Size = new Size(95, 20);
+            label4.TabIndex = 13;
+            label4.Text = "Export excel";
+            // 
+            // bt_export
+            // 
+            bt_export.Location = new Point(736, 103);
+            bt_export.Name = "bt_export";
+            bt_export.Size = new Size(100, 41);
+            bt_export.TabIndex = 12;
+            bt_export.Text = "Export";
+            bt_export.UseVisualStyleBackColor = true;
+            // 
+            // bt_search
+            // 
+            bt_search.Location = new Point(736, 12);
+            bt_search.Name = "bt_search";
+            bt_search.Size = new Size(100, 40);
+            bt_search.TabIndex = 11;
+            bt_search.Text = "Search";
+            bt_search.UseVisualStyleBackColor = true;
+            bt_search.Click += bt_search_Click;
+            // 
+            // tb_s_date
+            // 
+            tb_s_date.Location = new Point(426, 48);
+            tb_s_date.Name = "tb_s_date";
+            tb_s_date.Size = new Size(147, 26);
+            tb_s_date.TabIndex = 10;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(265, 54);
+            label3.Name = "label3";
+            label3.Size = new Size(131, 20);
+            label3.TabIndex = 9;
+            label3.Text = "Search with Date";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(265, 89);
+            label2.Name = "label2";
+            label2.Size = new Size(138, 20);
+            label2.TabIndex = 8;
+            label2.Text = "Search with Name";
+            // 
+            // tb_s_name
+            // 
+            tb_s_name.Location = new Point(426, 86);
+            tb_s_name.Name = "tb_s_name";
+            tb_s_name.Size = new Size(147, 26);
+            tb_s_name.TabIndex = 7;
+            // 
+            // bt_s_name
+            // 
+            bt_s_name.Location = new Point(604, 86);
+            bt_s_name.Name = "bt_s_name";
+            bt_s_name.Size = new Size(100, 30);
+            bt_s_name.TabIndex = 6;
+            bt_s_name.Text = "Name";
+            bt_s_name.UseVisualStyleBackColor = true;
+            bt_s_name.Click += bt_s_name_Click;
+            // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(265, 61);
+            label1.Location = new Point(265, 124);
             label1.Name = "label1";
-            label1.Size = new Size(67, 20);
+            label1.Size = new Size(113, 20);
             label1.TabIndex = 5;
-            label1.Text = "Input ID";
+            label1.Text = "Search with ID";
             // 
-            // button2
+            // bt_s_id
             // 
-            button2.Location = new Point(263, 120);
-            button2.Name = "button2";
-            button2.Size = new Size(102, 39);
-            button2.TabIndex = 4;
-            button2.Text = "ID";
-            button2.UseVisualStyleBackColor = true;
+            bt_s_id.Location = new Point(604, 123);
+            bt_s_id.Name = "bt_s_id";
+            bt_s_id.Size = new Size(100, 30);
+            bt_s_id.TabIndex = 4;
+            bt_s_id.Text = "ID";
+            bt_s_id.UseVisualStyleBackColor = true;
+            bt_s_id.Click += bt_s_id_Click;
             // 
-            // textBox1
+            // tb_s_id
             // 
-            textBox1.Location = new Point(265, 88);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 26);
-            textBox1.TabIndex = 3;
-            textBox1.TextChanged += textBox1_TextChanged;
+            tb_s_id.Location = new Point(426, 123);
+            tb_s_id.Name = "tb_s_id";
+            tb_s_id.Size = new Size(147, 26);
+            tb_s_id.TabIndex = 3;
+            tb_s_id.TextChanged += textBox1_TextChanged;
             // 
-            // button1
+            // bt_s_date
             // 
-            button1.Location = new Point(263, 9);
-            button1.Name = "button1";
-            button1.Size = new Size(100, 37);
-            button1.TabIndex = 2;
-            button1.Text = "Date";
-            button1.UseVisualStyleBackColor = true;
+            bt_s_date.Location = new Point(604, 46);
+            bt_s_date.Name = "bt_s_date";
+            bt_s_date.Size = new Size(100, 30);
+            bt_s_date.TabIndex = 2;
+            bt_s_date.Text = "Date";
+            bt_s_date.UseVisualStyleBackColor = true;
+            bt_s_date.Click += bt_s_date_Click;
             // 
             // monthCalendar1
             // 
             monthCalendar1.Location = new Point(8, 9);
             monthCalendar1.Name = "monthCalendar1";
             monthCalendar1.TabIndex = 1;
+            monthCalendar1.DateChanged += monthCalendar1_DateChanged;
             // 
             // dataGridView1
             // 
@@ -154,8 +277,19 @@
         private MonthCalendar monthCalendar1;
         private DataGridView dataGridView1;
         private Label label1;
-        private Button button2;
-        private TextBox textBox1;
-        private Button button1;
+        private Button bt_s_id;
+        private TextBox tb_s_id;
+        private Button bt_s_date;
+        private Label label2;
+        private TextBox tb_s_name;
+        private Button bt_s_name;
+        private ComboBox cb_month;
+        private Label label5;
+        private Label label4;
+        private Button bt_export;
+        private Button bt_search;
+        private TextBox tb_s_date;
+        private Label label3;
+        private Button bt_s_month;
     }
 }
--- FingerPrinter/Forms/TimeSheet.cs
+++ FingerPrinter/Forms/TimeSheet.cs
@@ -9,16 +9,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace FingerPrinter.Forms
 {
     public partial class TimeSheet : Form
     {
         private string employee_db_path = Main.employeeDatabase;
+        private bool is_select_month = false;
+        private bool is_select_date = false;
+        private bool is_select_name = false;
+        private bool is_selcet_id = false;
+        private string query = @"
+                        SELECT 
+                            Timesheet.EmployeePrivateID, 
+                            Employees.Name, 
+                            Timesheet.Date, 
+                            Timesheet.Time, 
+                            Timesheet.Type
+                        FROM 
+                            Timesheet
+                        JOIN 
+                            Employees 
+                        ON 
+                            Timesheet.EmployeePrivateID = Employees.PrivateID";
         public TimeSheet()
         {
             InitializeComponent();
-            LoadDataFromDatabase();
+            LoadDataFromDatabase(query, "normal");
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -38,7 +57,7 @@ namespace FingerPrinter.Forms
         {
 
         }
-        private void LoadDataFromDatabase()
+        private void LoadDataFromDatabase(string custom_query, string mode)
         {
             string connectionString = $"Data Source={employee_db_path};Version=3;";
 
@@ -47,22 +66,28 @@ namespace FingerPrinter.Forms
                 connection.Open();
 
                 //string query = "SELECT * FROM Timesheet";
-                string query = @"
-                                SELECT 
-                                    Timesheet.EmployeePrivateID, 
-                                    Employees.Name, 
-                                    Timesheet.Date, 
-                                    Timesheet.Time, 
-                                    Timesheet.Type
-                                FROM 
-                                    Timesheet
-                                JOIN 
-                                    Employees 
-                                ON 
-                                    Timesheet.EmployeePrivateID = Employees.PrivateID";
 
-                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                using (SQLiteCommand command = new SQLiteCommand(custom_query, connection))
                 {
+                    if(mode == "search")
+                    {
+                        if(is_selcet_id)
+                        {
+                            command.Parameters.AddWithValue("@ID", tb_s_id.Text);
+                        }
+                        if(is_select_date)
+                        {
+                            command.Parameters.AddWithValue("@Date", monthCalendar1.SelectionStart.ToString("yyyy-MM-dd"));
+                        }
+                        if(is_select_month)
+                        {
+                            command.Parameters.AddWithValue("@Month", value: cb_month.SelectedItem.ToString().PadLeft(2, '0')); // Ensure 2-digit month
+                        }
+                        if(is_select_name)
+                        {
+                            command.Parameters.AddWithValue("@Name", "%" + tb_s_name.Text + "%");
+                        }
+                    }
                     using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(command))
                     {
                         DataTable dataTable = new DataTable();
@@ -77,11 +102,139 @@ namespace FingerPrinter.Forms
                         {
                             dataGridView1.Columns["Time"].DefaultCellStyle.Format = "HH:mm:ss";
                         }
-                        if (dataGridView1.Columns["Date" ] != null)
+                        if (dataGridView1.Columns["Date"] != null)
                         {
                             dataGridView1.Columns["Date"].DefaultCellStyle.Format = "dd/MM/yyyy";
                         }
                     }
+                }
+            }
+        }
+
+        private void bt_search_Click(object sender, EventArgs e)
+        {
+            string search_query = @"
+                                    SELECT 
+                                        Timesheet.EmployeePrivateID, 
+                                        Employees.Name, 
+                                        Timesheet.Date, 
+                                        Timesheet.Time, 
+                                        Timesheet.Type
+                                    FROM 
+                                        Timesheet
+                                    JOIN 
+                                        Employees 
+                                    ON 
+                                        Timesheet.EmployeePrivateID = Employees.PrivateID
+                                    WHERE 1=1";
+            if (is_selcet_id)
+            {
+                search_query += " AND Timesheet.EmployeePrivateID = @ID";
+            } 
+            else if (is_select_date)
+            {
+                search_query += " AND Timesheet.Date = @Date";
+            }
+            else if (is_select_month)
+            {
+                search_query += "AND strftime('%m', Timesheet.Date) = @Month";
+            }
+            else if (is_select_name)
+            {
+                search_query += " AND Employees.Name LIKE @Name";
+            }
+            else
+            {
+
+            }
+            LoadDataFromDatabase(search_query, "search");
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            tb_s_date.Text = monthCalendar1.SelectionStart.ToString("dd/MM/yyyy");
+        }
+
+        private void bt_s_month_Click(object sender, EventArgs e)
+        {
+            if(is_select_date)
+            {
+                MessageBox.Show("Only date or month select in the time");
+            }
+            else
+            {
+                if (!is_select_month)
+                {
+                    is_select_month = true;
+                    bt_s_month.BackColor = Color.FromArgb(143, 228, 143);
+                }
+                else
+                {
+                    is_select_month = false;
+                    bt_s_month.BackColor = Color.Transparent;
+                }
+            }
+        }
+
+        private void bt_s_date_Click(object sender, EventArgs e)
+        {
+            if(is_select_month)
+            {
+                MessageBox.Show("Only date or month select in the time");
+            }
+            else
+            {
+                if (is_select_date)
+                {
+                    is_select_date = false;
+                    bt_s_date.BackColor = Color.Transparent;
+                }
+                else
+                {
+                    is_select_date = true;
+                    bt_s_date.BackColor = Color.FromArgb(143, 228, 143);
+                }
+            }
+        }
+
+        private void bt_s_name_Click(object sender, EventArgs e)
+        {
+            if(is_selcet_id)
+            {
+                MessageBox.Show("Only name or id select in the time");
+            }
+            else
+            {
+                if (is_select_name)
+                {
+                    is_select_name = false;
+                    bt_s_name.BackColor = Color.Transparent;
+                }
+                else
+                {
+                    is_select_name = true;
+                    bt_s_name.BackColor = Color.FromArgb(143, 228, 143);
+                }
+            }
+        }
+
+        private void bt_s_id_Click(object sender, EventArgs e)
+        {
+            if(is_select_name)
+            {
+                MessageBox.Show("Only name or id select in the time");
+            }
+            else
+            {
+                if (is_selcet_id)
+                {
+                    is_selcet_id = false;
+                    bt_s_id.BackColor = Color.Transparent;
+                }
+                else
+                {
+                    is_selcet_id = true;
+                    bt_s_id.BackColor = Color.FromArgb(143, 228, 143);
                 }
             }
         }
